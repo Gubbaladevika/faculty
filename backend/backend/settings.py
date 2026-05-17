7e135ae3eb9e30eb9490467f05c8d32808115dff
Devika <gubbaladevika65@gmail.com>
2026-05-17 20:44:32 +0530

    fixed cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,11 +93,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
 CORS_ALLOW_CREDENTIALS = True
 
 
